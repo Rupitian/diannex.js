f91dcb27c11b96eaa5214b87ff13e5e1f010a83e
MadCreativity <ahlgreenmadsen@gmail.com>
2021-07-15 13:32:37 +0200

    Fix formatting issue. Implement FunctionHandler.

--- src/classes/FunctionHandler.ts
+++ src/classes/FunctionHandler.ts
@@ -1,5 +1,33 @@
-class FunctionHandler {
+import Value from "types/Value";
 
+type DiannexFunc = (args: Value[]) => Value;
+
+class FunctionHandler {
+    #funcs: Record<string, DiannexFunc>;
+    
+    constructor(funcs: Record<string, DiannexFunc> = {}) {
+        this.#funcs = funcs;
+    }
+
+    invoke(name: string, args: Value[]): Value {
+        if (this.#funcs[name]) {
+            return this.#funcs[name](args);
+        }
+
+        throw new Error("Invalid function.");
+    }
+
+    registerFunction(name: string, func: DiannexFunc): void {
+        this.#funcs[name] = func;
+    }
+
+    unregisterFunction(name: string): void {
+        delete this.#funcs[name];
+    }
+
+    clear(): void {
+        this.#funcs = {};
+    }
 }
 
 export default FunctionHandler;
