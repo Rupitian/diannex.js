987ce23b0bb13d2c4541976284ece00416f46bc5
MadCreativity <ahlgreenmadsen@gmail.com>
2021-07-15 23:23:00 +0200

    Replace ts-node testing with proper ts-jest.

--- src/tests/index.ts
+++
@@ -1,7 +0,0 @@
-import * as path from "path";
-
-import Binary from "classes/Binary";
-
-const bin = Binary.readFromFile(path.join(process.cwd(), "files", "general-v3.dxb"));
-
-console.dir(bin, { depth: 4 });
